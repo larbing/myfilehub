3e62ed1d8e442e9bf185b49c924a67b55b1dfe36
larbing <larbing@163.com>
2024-07-17 15:27:36 +0800

    up

--- publish.sh
+++ publish.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
 # 定义变量
+GIT_REPO_URL="https://github.sonainai.com/larbing/myfilehub.git"
+PROJECT_DIR="/home/rock/git/myfilehub"
 DOCKER_CONTAINER_NAME="myfilehub"
 DOCKER_IMAGE_NAME="abc7223/myfilehub"
 DOCKER_TAG="latest" # 或者指定版本号，如 "v1.0"
+
+# 函数：删除现有项目目录
+delete_existing_project_dir() {
+    echo "删除现有项目目录..."
+    rm -rf $PROJECT_DIR
+}
+
+
+pull_git_repo() {
+    echo "更新Git仓库..."
+    cd $PROJECT_DIR && git pull
+}
+
+# 函数：克隆Git仓库
+clone_git_repo() {
+    echo "克隆Git仓库..."
+    git clone $GIT_REPO_URL $PROJECT_DIR
+}
+
+# 函数：构建Docker镜像
+build_docker_image() {
+    echo "构建Docker镜像..."
+    cd $PROJECT_DIR || exit
+    docker  build -t $DOCKER_IMAGE_NAME:$DOCKER_TAG .
+}
 
 # 函数：停止并移除旧容器
 stop_and_remove_old_container() {
@@ -11,42 +38,47 @@ stop_and_remove_old_container() {
     docker stop $DOCKER_CONTAINER_NAME && docker rm $DOCKER_CONTAINER_NAME
 }
 
-# 函数：拉取最新的Docker镜像
-pull_latest_image() {
-    echo "拉取最新的Docker镜像..."
-    docker pull $DOCKER_IMAGE_NAME:$DOCKER_TAG
-}
-
 # 函数：使用新镜像启动新的容器
 run_new_container() {
     echo "使用新镜像启动新的容器..."
     # 这里添加您启动容器所需的参数，例如端口映射、环境变量等
-    docker run --rm -d -v /data:/data --network host --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME:$DOCKER_TAG
+    docker run -d  -v /data/www:/data --network host --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME:$DOCKER_TAG
 }
 
 # 主程序
 echo "开始更新Docker应用..."
 
 
-clean_container(){
-        # 检查容器是否存在
-        if [ "$(docker ps -a -q -f name=$DOCKER_CONTAINER_NAME)" ]; then
-                echo "找到名为 $DOCKER_CONTAINER_NAME 的容器，准备更新..."
-                stop_and_remove_old_container
-        else
-                echo "未找到名为 $DOCKER_CONTAINER_NAME 的容器，将直接启动新容器..."
-        fi
-}
+if [ -d "$PROJECT_DIR" ]; then
+    pull_git_repo
+else
+    clone_git_repo
+fi
 
-pull_latest_image
-
-# 检查镜像拉取是否成功
+# 检查仓库克隆是否成功
 if [ $? -eq 0 ]; then
-    echo "镜像拉取成功，准备启动新容器..."
-    clean_container
+    echo "仓库克隆成功，准备构建Docker镜像..."
+    build_docker_image
+else
+    echo "仓库克隆失败，请检查网络连接和Git仓库URL。"
+    exit 1
+fi
+
+# 检查镜像构建是否成功
+if [ $? -eq 0 ]; then
+    echo "Docker镜像构建成功，准备更新容器..."
+
+    # 停止并移除旧容器
+    if [ "$(docker ps -a -q -f name=$DOCKER_CONTAINER_NAME)" ]; then
+        stop_and_remove_old_container
+    else
+        echo "未找到名为 $DOCKER_CONTAINER_NAME 的容器，将直接启动新容器..."
+    fi
+
+    # 启动新容器
     run_new_container
 else
-    echo "镜像拉取失败，请检查网络连接和镜像名称。"
+    echo "Docker镜像构建失败，请检查Dockerfile。"
     exit 1
 fi
 
